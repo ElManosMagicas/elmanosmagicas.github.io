97ad8cd2e9f5dfefd813379c729b837ec208fcb8
Juan Garay <ing.juandgarayc@gmail.com>
2023-05-06 10:35:53 -0500

    feat(toggle-btn): toggle speakers and animal states when speakers btn are clicked

--- main.js
+++ main.js
@@ -30,3 +30,49 @@ window.onload = () => {
     forestSection.scrollIntoView({ behavior: 'smooth' });
   });
 };
+
+let interactiveMirlaSoundBtn = document.getElementById('interactive-mirla-sound-btn');
+let interactiveMirlaSpeakers = document.getElementById('mirla-speakers');
+let interactiveMirlaState = document.getElementById('interactive-mirla-state-1');
+let isInteractiveMirlaSpeakers = true;
+let interactiveTucanSoundBtn = document.getElementById('interactive-tucan-sound-btn');
+let interactiveTucanSpeakers = document.getElementById('tucan-speakers');
+let interactiveTucanState = document.getElementById('interactive-tucan-state-1');
+let isInteractiveTucanSpeakers = true;
+let interactiveSnakeSoundBtn = document.getElementById('interactive-snake-sound-btn');
+let interactiveSnakeSpeakers = document.getElementById('snake-speakers');
+let interactiveSnakeState = document.getElementById('interactive-snake-state-1');
+let isInteractiveSnakeSpeakers = true;
+
+interactiveMirlaSoundBtn.addEventListener('click', () => {
+  isInteractiveMirlaSpeakers = !isInteractiveMirlaSpeakers;
+  if (isInteractiveMirlaSpeakers) {
+    interactiveMirlaSpeakers.src = './assets/escena-2-interacciones/boton-sonido-apagado.png';
+    interactiveMirlaState.src = './assets/escena-2-interacciones/1-mirla-estado-1.png';
+  } else {
+    interactiveMirlaSpeakers.src = './assets/escena-2-interacciones/boton-sonido-encendido.png';
+    interactiveMirlaState.src = './assets/escena-2-interacciones/1-mirla-estado-2.png';
+  }
+});
+
+interactiveTucanSoundBtn.addEventListener('click', () => {
+  isInteractiveTucanSpeakers = !isInteractiveTucanSpeakers;
+  if (isInteractiveTucanSpeakers) {
+    interactiveTucanSpeakers.src = './assets/escena-2-interacciones/boton-sonido-apagado.png';
+    interactiveTucanState.src = './assets/escena-2-interacciones/2-tucan-estado-1.png';
+  } else {
+    interactiveTucanSpeakers.src = './assets/escena-2-interacciones/boton-sonido-encendido.png';
+    interactiveTucanState.src = './assets/escena-2-interacciones/2-tucan-estado-2.png';
+  }
+});
+
+interactiveSnakeSoundBtn.addEventListener('click', () => {
+  isInteractiveSnakeSpeakers = !isInteractiveSnakeSpeakers;
+  if (isInteractiveSnakeSpeakers) {
+    interactiveSnakeSpeakers.src = './assets/escena-2-interacciones/boton-sonido-apagado.png';
+    interactiveSnakeState.src = './assets/escena-2-interacciones/2-serpiente-estado-1.png';
+  } else {
+    interactiveSnakeSpeakers.src = './assets/escena-2-interacciones/boton-sonido-encendido.png';
+    interactiveSnakeState.src = './assets/escena-2-interacciones/2-serpiente-estado-2.gif';
+  }
+});
