c8622962d55e6145b7b3902f739439ab4a023327
Juan Garay <ing.juandgarayc@gmail.com>
2023-05-05 23:05:34 -0500

    feat(scroll): add logic to horizontal scroll between sections

--- main.js
+++ main.js
@@ -175,3 +175,31 @@
 //     introTurtle.style.top = `100%`;
 //   }
 // });
+
+window.onload = () => {
+  let mapNextBtn = document.getElementById('map-next-btn');
+  let forestSection = document.getElementById('forest-section');
+
+  mapNextBtn.addEventListener('click', () => {
+    forestSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  let forestNextBtn = document.getElementById('forest-next-btn');
+  let interactiveSection = document.getElementById('interactive-section');
+
+  forestNextBtn.addEventListener('click', () => {
+    interactiveSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  let forestBackBtn = document.getElementById('forest-back-btn');
+  let mapSection = document.getElementById('map-section');
+
+  forestBackBtn.addEventListener('click', () => {
+    mapSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  let interactiveBackBtn = document.getElementById('interactive-back-btn');
+  interactiveBackBtn.addEventListener('click', () => {
+    forestSection.scrollIntoView({ behavior: 'smooth' });
+  });
+};
