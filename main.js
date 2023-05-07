fec8e0b5581c36d3fac4a757288313e6f438d912
Juan Garay <ing.juandgarayc@gmail.com>
2023-05-07 10:46:24 -0500

    feat(animations): add animations for the forest section

--- main.js
+++ main.js
@@ -30,3 +30,113 @@ window.onload = () => {
     forestSection.scrollIntoView({ behavior: 'smooth' });
   });
 };
+const forestSectionElement = document.querySelector('#forest-section');
+const forestRightTree = forestSectionElement.querySelector('#forest-right-tree');
+const forestLeftTree = forestSectionElement.querySelector('#forest-left-tree');
+const forestRightGif = forestSectionElement.querySelector('#forest-right-gif');
+const forestLeftGif = forestSectionElement.querySelector('#forest-left-gif');
+const forestCuyo = forestSectionElement.querySelector('#forest-cuyo');
+const forestTurtle = forestSectionElement.querySelector('#forest-turtle');
+const forestTingua = forestSectionElement.querySelector('#forest-tingua');
+const forestText1 = forestSectionElement.querySelector('#forest-text-1');
+const forestText2 = forestSectionElement.querySelector('#forest-text-2');
+
+const forestRightTreeObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-right-tree');
+      }, 250);
+    }
+  });
+});
+
+const forestLeftTreeObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-left-tree');
+      }, 500);
+    }
+  });
+});
+
+const forestRightGifObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-right-gif');
+      }, 500);
+    }
+  });
+});
+
+const forestLeftGifObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-left-gif');
+      }, 500);
+    }
+  });
+});
+
+const forestCuyoObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-cuyo');
+      }, 750);
+    }
+  });
+});
+
+const forestTurtleObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-turtle');
+      }, 750);
+    }
+  });
+});
+
+const forestTinguaObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-tingua');
+      }, 750);
+    }
+  });
+});
+
+const forestText1Observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-text-1');
+      }, 2000);
+    }
+  });
+});
+
+const forestText2Observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-text-2');
+      }, 9000);
+    }
+  });
+});
+
+forestRightTreeObserver.observe(forestRightTree);
+forestLeftTreeObserver.observe(forestLeftTree);
+forestRightGifObserver.observe(forestRightGif);
+forestLeftGifObserver.observe(forestLeftGif);
+forestCuyoObserver.observe(forestCuyo);
+forestTurtleObserver.observe(forestTurtle);
+forestTinguaObserver.observe(forestTingua);
+forestText1Observer.observe(forestText1);
+forestText2Observer.observe(forestText2);
