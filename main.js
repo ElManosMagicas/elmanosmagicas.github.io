bd70959332ff1dd28e4184de06e5be38c2e6083b
Juan Garay <ing.juandgarayc@gmail.com>
2023-05-07 12:05:44 -0500

    feat(animation): add animations to interactive section and add text

--- main.js
+++ main.js
@@ -140,3 +140,154 @@ forestTurtleObserver.observe(forestTurtle);
 forestTinguaObserver.observe(forestTingua);
 forestText1Observer.observe(forestText1);
 forestText2Observer.observe(forestText2);
+
+const interactiveSectionElement = document.querySelector('#interactive-section');
+const interactiveLeftTreeContainer = interactiveSectionElement.querySelector(
+  '#interactive-left-tree-container'
+);
+const interactiveMirla = interactiveSectionElement.querySelector('#interactive-mirla');
+const interactiveRightTreeContainer = interactiveSectionElement.querySelector(
+  '#interactive-right-tree-container'
+);
+const interactiveTucan = interactiveSectionElement.querySelector('#interactive-tucan');
+const interactiveSnake = interactiveSectionElement.querySelector('#interactive-snake');
+const interactiveTingua = interactiveSectionElement.querySelector('#interactive-tingua');
+const interactiveTurtle = interactiveSectionElement.querySelector('#interactive-turtle');
+const interactiveCuyo = interactiveSectionElement.querySelector('#interactive-cuyo');
+const interactiveText1 = interactiveSectionElement.querySelector('#interactive-text-1');
+const interactiveMirlaSoundBtn = interactiveSectionElement.querySelector('#interactive-text-1');
+const interactiveTucanSoundBtn = interactiveSectionElement.querySelector('#interactive-text-1');
+const interactiveSnakeSoundBtn = interactiveSectionElement.querySelector('#interactive-text-1');
+
+const interactiveLeftTreeContainerObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-left-tree-container');
+      }, 250);
+    }
+  });
+});
+
+const interactiveMirlaObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-mirla');
+      }, 1250);
+    }
+  });
+});
+
+const interactiveRightTreeContainerObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-right-tree-container');
+      }, 2000);
+    }
+  });
+});
+
+const interactiveTucanObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-tucan');
+      }, 2750);
+    }
+  });
+});
+
+const interactiveSnakeObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-snake');
+      }, 3000);
+    }
+  });
+});
+
+const interactiveTinguaObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-tingua');
+      }, 4200);
+    }
+  });
+});
+
+const interactiveTurtleObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-turtle');
+      }, 4400);
+    }
+  });
+});
+
+const interactiveCuyoObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-cuyo');
+      }, 4500);
+    }
+  });
+});
+
+const interactiveText1Observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-text-1');
+      }, 5500);
+    }
+  });
+});
+
+const interactiveMirlaSoundBtnObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-sound-btn');
+      }, 7000);
+    }
+  });
+});
+
+const interactiveTucanSoundBtnObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-sound-btn');
+      }, 7100);
+    }
+  });
+});
+
+const interactiveSnakeSoundBtnObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('appear-int-sound-btn');
+      }, 7200);
+    }
+  });
+});
+
+interactiveLeftTreeContainerObserver.observe(interactiveLeftTreeContainer);
+interactiveMirlaObserver.observe(interactiveMirla);
+interactiveRightTreeContainerObserver.observe(interactiveRightTreeContainer);
+interactiveTucanObserver.observe(interactiveTucan);
+interactiveSnakeObserver.observe(interactiveSnake);
+interactiveTinguaObserver.observe(interactiveTingua);
+interactiveTurtleObserver.observe(interactiveTurtle);
+interactiveCuyoObserver.observe(interactiveCuyo);
+interactiveText1Observer.observe(interactiveText1);
+interactiveMirlaSoundBtnObserver.observe(interactiveMirlaSoundBtn);
+interactiveTucanSoundBtnObserver.observe(interactiveTucanSoundBtn);
+interactiveSnakeSoundBtnObserver.observe(interactiveSnakeSoundBtn);
